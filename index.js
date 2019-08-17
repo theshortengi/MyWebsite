899bcd5ab8aa8edf9c9bc6e94ec65d0faefa08c4
theshortengi <theshortengi@gmail.com>
2019-08-16 20:21:33 -0400

    made three switches

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const randquote =document.getElementById("quote");
-const quotes = ["Not All Who Wander Are Lost - J. R. R. Tolkien","If a man does not keep pace with his companions, perhaps it is because he hears a different drummer. Let him step to the music which he hears, however measured or far away. - Henry David Thoreau", "quote 3"];
+const quotes = ["Not All Who Wander Are Lost - J. R. R. Tolkien",
+    "If a man does not keep pace with his companions, perhaps it is because he hears a different drummer. Let him step to the music which he hears, however measured or far away. - Henry David Thoreau",
+    ""];
 const quoteindex=Math.floor(Math.random()*quotes.length);
 randquote.textContent=quotes[quoteindex];
