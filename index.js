005932f7e94085df6b46a8176cdbd1df124fa454
theshortengi <theshortengi@gmail.com>
2020-09-08 21:51:10 -0400

    Add third quote

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const randquote =document.getElementById("quote");
 const quotes = ["Not All Who Wander Are Lost - J. R. R. Tolkien",
     "If a man does not keep pace with his companions, perhaps it is because he hears a different drummer. Let him step to the music which he hears, however measured or far away. - Henry David Thoreau",
-    ""];
+    "Non impediti ratione cogitationis - Tom Magliozzi"];
 const quoteindex=Math.floor(Math.random()*quotes.length);
 randquote.textContent=quotes[quoteindex];
 
